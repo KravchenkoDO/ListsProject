793ec285fe5aa06a8bb04bee3661cce695d59b7e
Dmitry <Kravchenkodmo@gmail.com>
2021-03-17 17:32:45 +0200

    Add methods body

--- ArrayListManualTest/Program.cs
+++ ArrayListManualTest/Program.cs
@@ -9,17 +9,18 @@ namespace ArrayListManualTest
         {
             var random = new Random();
             MyArrayList arList = new MyArrayList();
-            Console.WriteLine(arList.Length);
             for (int i = 0; i < 4; i++)
             {
                 arList.Add(random.Next(10, 21));
                 
             }
-            Console.WriteLine(arList.Length);
             arList.Print();
+            Console.WriteLine(arList.Count());
             arList.AddFirst(125);
+            Console.WriteLine(arList.Count());
             arList.Print();
             arList.Insert(3,999);
+            Console.WriteLine(arList.Count());
             arList.Print();
         }
     }
