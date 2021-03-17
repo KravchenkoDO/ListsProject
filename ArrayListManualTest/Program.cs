b07dd9fb26153fef1fe6d0ad6dd96c86df3d3353
Dmitry <Kravchenkodmo@gmail.com>
2021-03-17 07:17:54 +0200

    Initial new Project

---
+++ ArrayListManualTest/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using ListsProject;
+
+namespace ArrayListManualTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var random = new Random();
+            MyArrayList arList = new MyArrayList();
+            Console.WriteLine(arList.Length);
+            for (int i = 0; i < 4; i++)
+            {
+                arList.Add(random.Next(10, 21));
+                
+            }
+            Console.WriteLine(arList.Length);
+            arList.Print();
+            arList.AddFirst(125);
+            arList.Print();
+            arList.Insert(3,999);
+            arList.Print();
+        }
+    }
+}
---
+++ ListsProject/MyArrayList.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections;
+
+namespace ListsProject
+{
+    public class MyArrayList
+    {
+        public int Length { get; private set; }
+        
+        private int[] _array;
+
+        public MyArrayList()
+        {
+            Length = 0;
+
+            _array = new int[10];
+        }
+
+        public void Add(int value)
+        {
+            if (Length == _array.Length)
+            {
+                UpSize();
+            }
+            _array[Length] = value;
+            Length++;
+        }
+
+        public void AddFirst(int value)
+        {
+            if (Length == _array.Length)
+            {
+                UpSize();
+            }
+
+            int[] tmpArray = new int[_array.Length+1];
+            tmpArray[0] = value;
+            for (int i = 0; i < _array.Length; i++)
+            {
+                tmpArray[i+1] = _array[i];
+            }
+            _array = tmpArray;
+        }
+
+        public void Insert(int index, int value)
+        {
+            if (Length == _array.Length)
+            {
+                UpSize();
+            }
+
+
+
+        }
+        public void Print()
+        {
+            for (int i = 0; i < _array.Length; i++)
+            {
+                Console.WriteLine($"List<{i}> = {_array[i]}");
+            }
+        }
+
+        private void UpSize()
+        {
+            int newLength = (int) (_array.Length * 1.33d + 1);
+
+            int[] tmpArray = new int[newLength];
+
+            for (int i = 0; i < _array.Length; i++)
+            {
+                tmpArray[i] = _array[i];
+            }
+
+            _array = tmpArray;
+        }
+    }
+}
